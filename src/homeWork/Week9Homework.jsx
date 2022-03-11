c01a74b13942ce97c575b1a525817d64eaceab05
Leanne Rowe <leannemrowe@gmail.com>
2022-03-11 18:20:36 +0000

    Week9Homework managed to get an animation to work

--- src/homeWork/Week9Homework.jsx
+++ src/homeWork/Week9Homework.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import _NavBarHW from './_NavBarHW';
+import { motion } from "framer-motion"
+import './Week9Homeworkstyle.css';
+
 
 function Week9Homework() {
   return (
@@ -7,6 +10,20 @@ function Week9Homework() {
       <_NavBarHW />
       <div className="content">
         <h1>Week 9 Homework</h1>
+        <motion.button 
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          className="save-button" onClick={() => null}>
+          Launch modal
+        </motion.button>
+          <motion.div 
+          className="animationBox"
+          animate={{
+          scale: [1, 2, 2, 1, 1],
+          rotate: [0, 0, 270, 270, 0],
+          borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+        }}> HELLO ANIMATION
+        </motion.div>
 
       </div> 
     </div>
